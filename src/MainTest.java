5db5853514da241a61b4b77f444563fadd970b70
Anasshtbt <f135168630@taalim.ma>
2024-09-23 12:38:52 +0100

    first commit

---
+++ src/MainTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+    @Test
+    public void testMain() {
+        assertEquals("Hello, World!", "Hello, World!");
+    }
+}
